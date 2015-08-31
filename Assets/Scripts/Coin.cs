dbe6da6741ace31c162dcbf73b946033d481ba04
Nguyen Phan <nqphan212@gmail.com>
2015-08-31 14:02:30 -0400

    make the coins move up and down in Coin script

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,18 +6,47 @@ public class Coin : MonoBehaviour {
 	[SerializeField]
 	private float rotateSpeed = 1.0f; // In rotations per second
 	
+	[SerializeField]
+	private float floatSpeed = 0.5f; // In cycles (up and down) per second
+	
+	[SerializeField]
+	private float movementDistance = 0.5f; // The maximum distance the coin can move up and down
+	
+	private float startingY;
+	private bool isMovingUp = true;
+	
 	// Use this for initialization
 	void Start () {
-		transform.Rotate (transform.up, Random.Range(0f, 360f));
-		StartCoroutine (Spin ());
+		startingY = transform.position.y;
+		transform.Rotate (transform.up, Random.Range (0f, 360f));
 	}
 	
-	private IEnumerator Spin()
-	{
-		while (true)
-		{
-			transform.Rotate (transform.up, 360 * rotateSpeed * Time.deltaTime);
-			yield return 0;
-		}
+	void Update() {
+		Spin ();
+		Float ();
 	}
+	
+	private void Spin()
+	{
+		transform.Rotate (transform.up, 360 * rotateSpeed * Time.deltaTime);
+	}
+	
+	private void Float()
+	{
+		float newY = transform.position.y + (isMovingUp ? 1 : -1) * 2 * movementDistance * floatSpeed * Time.deltaTime;
+		
+		if (newY > startingY + movementDistance)
+		{
+			newY = startingY + movementDistance;
+			isMovingUp = false;
+		}
+		else if (newY < startingY)
+		{
+			newY = startingY;
+			isMovingUp = true;
+		}
+		
+		transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+	}
+	
 }
