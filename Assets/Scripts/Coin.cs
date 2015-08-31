d3c1858d5e53b4bfaca2206695b94e2520b7aaaa
Nguyen Phan <nqphan212@gmail.com>
2015-08-31 13:27:57 -0400

    Add a player and coins to the enviroment

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+	
+	[SerializeField]
+	private float rotateSpeed = 1.0f; // In rotations per second
+	
+	// Use this for initialization
+	void Start () {
+		transform.Rotate (transform.up, Random.Range(0f, 360f));
+		StartCoroutine (Spin ());
+	}
+	
+	private IEnumerator Spin()
+	{
+		while (true)
+		{
+			transform.Rotate (transform.up, 360 * rotateSpeed * Time.deltaTime);
+			yield return 0;
+		}
+	}
+}
